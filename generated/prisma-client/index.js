47b384fc52478c1eb9daabb6d665fdcc85768f1f
veera reddy obulareddy <veerareddy.obula@gmail.com>
2019-10-20 12:52:38 +0000

    Setting up Fin-Bus-Prisma

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://139.59.58.153:4466`
+});
+exports.prisma = new exports.Prisma();
